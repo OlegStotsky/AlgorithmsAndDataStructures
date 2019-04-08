25fff68e19ae254f07090dbfdfa5bc56b60c5892
Oleg Stotsky <ostotsky@gmail.com>
2019-04-08 19:27:01 +1000

    refactor functional linear search implementation

--- Search/LinearSearch/linearSearchFunctional.scala
+++ Search/LinearSearch/linearSearchFunctional.scala
@@ -1,3 +1,15 @@
 object main {
-  def search[T](xs: List[T], x: T): Option[T] = xs.find(_ == x)
+  def search[T](xs: List[T], x: T): Option[Int] = {
+    def searchIter[T](xs: List[T], i: Int, x: T): Option[Int] = xs match {
+      case Nil => None
+      case y :: ys => {
+        if (x == y) Some(i)
+        else {
+          searchIter(ys, i+1, x)
+        }
+      }
+    }
+
+    searchIter(xs, 0, x)
+  } 
 }
